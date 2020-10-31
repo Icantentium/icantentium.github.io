949bb7d73f23a13c2c7c89aba9cd3e9846904bc6
Icantentium <taylantz+icantentiumgithub@gmail.com>
2020-10-31 07:44:02 +0100

    Baselayout including base functions

---
+++ compute.js
@@ -0,0 +1,14 @@
+function cartesianProduct(...a) {
+    return a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())));
+}
+
+function calculateSetBonuses(equippedArtifacts, checkedGameplayFlags) {
+    let addedSettypeStats = null;
+    addedSettypeStats = "test";
+    return addedSettypeStats
+}
+
+function calculateTotalDamage(characterBaseStats, addedArtifactStats, addedWeaponStats, addedSettypeStats) {
+    let totalDamage = 0;
+    return totalDamage;
+}
